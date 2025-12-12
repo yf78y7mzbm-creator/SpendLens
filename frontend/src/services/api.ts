ba55333faf3a0c2ac58704e656863bf799ef07d3
yf78y7mzbm-creator <yf78y7mzbm@privaterelay.appleid.com>
2025-12-12 20:17:00 +0000

    Fix API base URL to use environment variable

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = '/api'
+const API_BASE = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api'
 
 export const api = {
   // Budgets
